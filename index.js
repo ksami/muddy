87bdb9d7644e92e19f640221a307e6053308d999
ksami <ksami.ihide@gmail.com>
2014-06-13 17:12:32 +0800

    read from json, skele for map and user

--- index.js
+++ index.js
@@ -1,25 +1,78 @@
 var port = 3000;
+var _fileindex = __dirname + '/index.html';
+var _filemaps = __dirname + '/maps.json';
+var _fileusers = __dirname + '/users.json';
 
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var fs = require('fs');
 
+var maps = {};
+var users = {};
+
+
+//Read data files
+fs.readFile(_filemaps, 'utf8', function (err, data) {
+	if(err) {
+		console.log('Map file error: ' + err);
+		return;
+	}
+	maps = JSON.parse(data);
+	console.dir(maps);
+})
+fs.readFile(_fileusers, 'utf8', function (err, data) {
+	if(err) {
+		console.log('User file error: ' + err);
+		return;
+	}
+	users = JSON.parse(data);
+	console.dir(users);
+})
+
+
+
+//Route handler
 app.get('/',function(req, res){
-	res.sendfile('index.html');
+	res.sendfile(_fileindex);
 });
 
+
+
 io.on('connection', function(socket){
+	
+	//When user first connects
 	socket.join(socket.id);
 	console.log('user ' + socket.id + ' connected');
 	console.log(socket.rooms);
-	io.to(socket.id).emit('messageBroad', 'Welcome!');
+	io.to(socket.id).emit('message', 'Welcome! Please login by typing your nick with @nick');
+	io.to(socket.id).emit('map', maps['0']);
 	
-	socket.on('message', function(msg){
+	//Bind nick and socket.id
+	socket.on('nick', function(nick){
+		users[nick] = {"nick": nick, "socketid": socket.id};
+		fs.writeFile(_fileusers, JSON.stringify(users, null, 4), function(err) {
+			if(err) {
+				console.log("User file error: " + err);
+			}
+			else {
+				console.log("Users.JSON save to " + _fileusers);
+				io.to(socket.id).emit('message', 'Your nick has been set to ' + nick);
+			}
+		})
+	})
+
+	//Any other input, echo back
+	socket.on('command', function(msg){
 		console.log(socket.id + ' sends: ' + msg);
-		io.to(socket.id).emit('messageBroad', msg);
+		io.to(socket.id).emit('message', msg);
 	});
 });
 
 http.listen(port, function(){
 	console.log('listening on *:' + port);
 });
+
+setInterval(function() {
+	io.emit('message', 'Remember to leave feedback at github.com/ksami/muddy');
+}, 60000);
